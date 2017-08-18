2cfdc5b3417b3fb5d6daee92cb491aa43748b912
Sorackb <sorackb@gmail.com>
2017-08-18 15:31:50 -0300

    CASE simplificado e alteração da data para padrão ANSI

--- MSSQL/Adiciona consulta WITH dentro de outra consulta - SQL Server (230269).sql
+++ MSSQL/Adiciona consulta WITH dentro de outra consulta - SQL Server (230269).sql
@@ -1,5 +1,5 @@
 /**
- * Utilizado na resposta para a pergunta "Adiciona consulta WITH dentro de outra consulta - SQL Server":
+ * Utilizado na resposta para a pergunta "Adiciona consulta WITH dentro de outra consulta . SQL Server":
  * https://pt.stackoverflow.com/a/230279/59479
  */
 
@@ -19,43 +19,44 @@ INSERT INTO solicitacao(soldatafechamento,
                         proid,
                         solestagioid,
                         soltipid)
-VALUES('26-06-2017', 3, 110, NULL),
-      ('26-06-2017', 4, 112, NULL),
-      ('27-06-2017', 4, 110, NULL),
-      ('27-06-2017', 1, 110, NULL),
-      ('27-06-2017', 4, 110, NULL),
-      ('27-06-2017', 4, 110, NULL),
-      ('27-06-2017', 4, 110, NULL),
-      ('27-06-2017', 4, NULL, 35),
-      ('27-06-2017', 4, NULL, 35),
-      ('28-06-2017', 4, NULL, 35),
-      ('28-06-2017', 4, NULL, 35),
-      ('28-06-2017', 4, NULL, 35),
-      ('28-06-2017', 4, NULL, 35),
-      ('28-06-2017', 4, NULL, 35),
-      ('29-06-2017', 4, 110, NULL),
-      ('29-06-2017', 4, 110, NULL),
-      ('29-06-2017', 4, NULL, 35),
-      ('29-06-2017', 4, NULL, 35),
-      ('29-06-2017', 4, NULL, 35),
-      ('29-06-2017', 4, NULL, 35),
-      ('29-06-2017', 4, 110, NULL),
-      ('29-06-2017', 4, 110, NULL),
-      ('29-06-2017', 4, 110, NULL);
+VALUES('26.06.2017', 3, 110, NULL),
+      ('26.06.2017', 4, 112, NULL),
+      ('27.06.2017', 4, 110, NULL),
+      ('27.06.2017', 1, 110, NULL),
+      ('27.06.2017', 4, 110, NULL),
+      ('27.06.2017', 4, 110, NULL),
+      ('27.06.2017', 4, 110, NULL),
+      ('27.06.2017', 4, NULL, 35),
+      ('27.06.2017', 4, NULL, 35),
+      ('28.06.2017', 4, NULL, 35),
+      ('28.06.2017', 4, NULL, 35),
+      ('28.06.2017', 4, NULL, 35),
+      ('28.06.2017', 4, NULL, 35),
+      ('28.06.2017', 4, NULL, 35),
+      ('29.06.2017', 4, 110, NULL),
+      ('29.06.2017', 4, 110, NULL),
+      ('29.06.2017', 4, NULL, 35),
+      ('29.06.2017', 4, NULL, 35),
+      ('29.06.2017', 4, NULL, 35),
+      ('29.06.2017', 4, NULL, 35),
+      ('29.06.2017', 4, 110, NULL),
+      ('29.06.2017', 4, 110, NULL),
+      ('29.06.2017', 4, 110, NULL);
 
 BEGIN TRY
    WITH dias AS(
-    SELECT CAST('2017-06-26' AS DATE) AS dia
+    SELECT CAST('2017.06.26' AS DATE) AS dia
     UNION ALL
     SELECT DATEADD(DAY, 1, d.dia)
       FROM dias d
-     WHERE d.dia < '2017-06-30'
+     WHERE d.dia < '2017.06.30'
   )
   SELECT CONVERT(VARCHAR, d.dia, 103) AS data_cancelado,
-         COUNT(CASE WHEN s.proid = 4 AND s.soltipid = 35 THEN 1 ELSE NULL END) AS abertos,
-         COUNT(CASE WHEN s.proid = 4 AND s.solestagioid = 110 THEN 1 ELSE NULL END) AS cancelados
+         COUNT(CASE WHEN s.soltipid = 35 THEN 1 ELSE NULL END) AS abertos,
+         COUNT(CASE WHEN s.solestagioid = 110 THEN 1 ELSE NULL END) AS cancelados
     FROM dias d
          LEFT JOIN solicitacao s ON CONVERT(DATE, s.soldatafechamento, 103) = d.dia
+                                AND s.proid = 4
   GROUP BY d.dia
   OPTION (MAXRECURSION 0);
 END TRY
